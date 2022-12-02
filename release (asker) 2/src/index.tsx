28810a64b36eaa79588abf711b2cd0238e9d8a2b
antonvh <antonhaninvl@gmail.com>
2022-12-02 12:19:51 +0300

    сборка создана wip

--- release (asker) 2/src/index.tsx
+++ release (asker) 2/src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+createRoot(document.getElementById('app')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-root.render(
-  <App />
-);
-
